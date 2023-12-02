20ebcd1f9e2c5a580e5e2ec379880f5669cca23e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-01 22:34:10 -0300

    Arquivo certo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string phoneWord, fone;
+
+Console.WriteLine("--- Conversor de Phone Words ---\n");
+
+Console.Write("Telefone...: ");
+phoneWord = Console.ReadLine()!;
+
+fone = phoneWord.ToLower()
+    .Replace("a", "2")
+    .Replace("b", "2")
+    .Replace("c", "2")
+
+    .Replace("d", "3")
+    .Replace("e", "3")
+    .Replace("f", "3")
+
+    .Replace("g", "4")
+    .Replace("h", "4")
+    .Replace("i", "4")
+
+    .Replace("j", "5")
+    .Replace("k", "5")
+    .Replace("l", "5")
+
+    .Replace("m", "6")
+    .Replace("n", "6")
+    .Replace("o", "6")
+
+    .Replace("p", "7")
+    .Replace("q", "7")
+    .Replace("r", "7")
+    .Replace("s", "7")
+
+    .Replace("t", "8")
+    .Replace("u", "8")
+    .Replace("v", "8")
+
+    .Replace("w", "9")
+    .Replace("x", "9")
+    .Replace("y", "9")
+    .Replace("z", "9")
+;
+
+Console.WriteLine($"\nO número digitado é {fone}.");
